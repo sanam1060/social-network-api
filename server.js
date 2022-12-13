c7c1188640b0d6998a2a488d4c9b28d6b5bdb168
sanam1060 <sanam_g1990@yahoo.com>
2022-12-13 15:05:47 -0800

    edit dependecies and add thought controllers

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(require("routes"));
+//app.use(express.static("public"));
+app.use(require("./routes"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/social-network-api",
+  process.env.MONGODB_URI || "mongodb://localhost/social-network-api",
   {
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedToplogy: true,
   }
@@ -19,6 +20,4 @@ mongoose.connect(
 
 mongoose.set("debug", true);
 
-app.listen(PORT, () => {
-  console.log(`Connected on ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Connected on http://localhost:${PORT}`));
